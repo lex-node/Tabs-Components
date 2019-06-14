2b9cc16834c80b8799c5be2276970c8d0598c807
lex_node <gabriel.j.shapiro@gmail.com>
2019-06-13 22:36:51 -0400

    adds tab expansion functionality, but with a strange glitch that causes multiple tabs to expand simultaneously

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,4 +1,3 @@
-
 class TabItem {
   constructor(element) {
     // Assigns this.element to the passed in element
@@ -7,21 +6,21 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items = document.querySelectorAll('.tabs-item');
 
-    // Remove the class "tabs-item-selected" from each element
+    // Removes the class "tabs-item-selected" from each element
+    Array.from(items).forEach(item => item.classList.toggle("tabs-item-selected"));
 
-    // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    // Adds a class named "tabs-item-selected" to this element
+    this.element.classList.toggle("tabs-item-selected");
   }
 }
-
 
 class TabLink {
   constructor(element) {
     // Assigns this.element to the passed in DOM element
     this.element = element;
-    
+
     // Gets the custom data attribute on the Link
     this.data = this.element.dataset.tab;
     
@@ -32,23 +31,25 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener("click", => this.select());
+    this.element.addEventListener("click", () => this.select());
   };
 
   select() {
-    // Get all of the elements with the tabs-link class
-    // const links;
+    // Gets all of the elements with the tabs-link class
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach(link =>  link.classList.toggle('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.toggle("tabs-link-selected");
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
+
+
 
 
 
